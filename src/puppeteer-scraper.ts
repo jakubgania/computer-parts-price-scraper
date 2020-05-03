8d3b37fc0d2143fac7f6831a30115a5b8caa5dda
jakubgania <contact@jakubgania.io>
2020-05-03 12:16:23 +0200

    update

--- src/puppeteer-scraper.ts
+++ src/puppeteer-scraper.ts
@@ -21,7 +21,7 @@ export class PuppeteerScraper {
                 break;
             }
             default: {
-                console.log('not found')
+                return null;
                 break;
             }
         }
--- test/puppeteer-scraper.test.ts
+++ test/puppeteer-scraper.test.ts
@@ -1,7 +1,37 @@
 import { PuppeteerScraper } from "../src/puppeteer-scraper";
 
-it('works', () => {
+it('new PuppeteerScraper()', () => {
     const obj = new PuppeteerScraper();
 
     expect(obj.processingType).toBe('sequential');
-})
+});
+
+it('new PuppeteerScraper("parallel")', () => {
+    const obj = new PuppeteerScraper('parallel');
+
+    expect(obj.processingType).toBe('parallel');
+});
+
+it('new PuppeteerScraper("parallel-limits")', () => {
+    const obj = new PuppeteerScraper('parallel-limits');
+
+    expect(obj.processingType).toBe('parallel-limits');
+});
+
+it('return null => new PuppeteerScraper("sequentl")', () => {
+    const obj = new PuppeteerScraper('sequentl');
+
+    expect(obj.run()).toBe(null);
+});
+
+it('return null => new PuppeteerScraper("parall")', () => {
+    const obj = new PuppeteerScraper('parall');
+
+    expect(obj.run()).toBe(null);
+});
+
+it('return null => new PuppeteerScraper("parallel-limit")', () => {
+    const obj = new PuppeteerScraper('parallel-limit');
+
+    expect(obj.run()).toBe(null);
+});
